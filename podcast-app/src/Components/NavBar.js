ee0f8bcb56adc8439d4065e1e862d1bfd5d834bc
Diana <mukharidiana@gmail.com>
2024-06-13 12:45:24 +0200

    navigation bar component created

---
+++ podcast-app/src/Components/NavBar.js
@@ -0,0 +1,14 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/genres">Genres</Link>
+        </nav>
+    );
+};
+
+export default Navbar;
