8990ef4998896070e11924f2cbf404c9d5a40ce2
Diana <mukharidiana@gmail.com>
2024-06-18 13:15:12 +0200

    html

--- podcast-app/src/App.js
+++ podcast-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '.Components/pages/Home';
+import Home from './Components/pages/Home';
 //import About from './Components/Pages/Genre';
 //import Dashboard from './Components/Pages/Podcast';
 import Podcast from './Components/Pages/Podcast';
